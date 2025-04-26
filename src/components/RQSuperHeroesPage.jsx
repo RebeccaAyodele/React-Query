92f54a00a5446a6038cb8a26d409a03ff853beea
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-04-26 16:37:43 +0100

    Callback onError and onSuccess

--- src/components/RQSuperHeroesPage.jsx
+++ src/components/RQSuperHeroesPage.jsx
@@ -1,15 +1,29 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import { useEffect } from "react"
 
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
 
 export const RQSuperHeroesPage = () => {
+
+
   const { isLoading, error, data, isFetching, refetch } = useQuery({
     queryKey: ['super-heroes'],
     queryFn: fetchSuperHeroes,
-    enabled: false,
+  })
+
+  useEffect(() => {
+    if (data) {
+      console.log('Perform side effect after data fetching', data);
+    }
+  })
+
+  useEffect(() => {
+    if (error) {
+      console.log('Perform side effet after encountering error', error);
+    }
   })
 
   console.log({ isLoading, isFetching });
